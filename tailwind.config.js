beb3d1af65073ab80876c4981f40e50daf9f8570
anindodas <125241429+anindodas@users.noreply.github.com>
2024-10-12 14:28:00 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,7 @@ module.exports = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: "class",
   theme: {
     container: {
       center: true,
@@ -15,11 +16,22 @@ module.exports = {
 
     screens: {
       xs: "450px",
+      // => @media (min-width: 450px) { ... }
+
       sm: "575px",
+      // => @media (min-width: 576px) { ... }
+
       md: "768px",
+      // => @media (min-width: 768px) { ... }
+
       lg: "992px",
+      // => @media (min-width: 992px) { ... }
+
       xl: "1200px",
+      // => @media (min-width: 1200px) { ... }
+
       "2xl": "1400px",
+      // => @media (min-width: 1400px) { ... }
     },
     extend: {
       colors: {
@@ -27,20 +39,21 @@ module.exports = {
         transparent: "transparent",
         white: "#FFFFFF",
         black: "#121723",
+        dark: "#1D2430",
         primary: "#000000",
         yellow: "#FBB040",
-        "bg-color-dark": "#171C28", // Optionally remove dark color references if not needed
+        "bg-color-dark": "#171C28",
         "body-color": {
           DEFAULT: "#788293",
-          dark: "#959CB1", // Remove or adjust this
+          dark: "#959CB1",
         },
         stroke: {
           stroke: "#E3E8EF",
-          dark: "#353943", // Remove or adjust this
+          dark: "#353943",
         },
         gray: {
           ...colors.gray,
-          dark: "#1E232E", // Remove or adjust this
+          dark: "#1E232E",
           light: "#F0F2F9",
         },
       },
@@ -51,8 +64,10 @@ module.exports = {
         two: "0px 5px 10px rgba(6, 8, 15, 0.1)",
         three: "0px 5px 15px rgba(6, 8, 15, 0.05)",
         sticky: "inset 0 -1px 0 0 rgba(0, 0, 0, 0.1)",
+        "sticky-dark": "inset 0 -1px 0 0 rgba(255, 255, 255, 0.1)",
         "feature-2": "0px 10px 40px rgba(48, 86, 211, 0.12)",
         submit: "0px 5px 20px rgba(4, 10, 34, 0.1)",
+        "submit-dark": "0px 5px 20px rgba(4, 10, 34, 0.1)",
         btn: "0px 1px 2px rgba(4, 10, 34, 0.15)",
         "btn-hover": "0px 1px 2px rgba(0, 0, 0, 0.15)",
         "btn-light": "0px 1px 2px rgba(0, 0, 0, 0.1)",
